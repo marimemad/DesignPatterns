79dba65ff161f4aaf4f5baea6e0d83f04b61797b
marimemad-DrGreiche <marim.emad@drgreiche>
2024-01-23 13:59:09 +0200

    TemplatePattern

---
+++ TemplatePattern/TempletePattern/TempletePattern/HouseTemplate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TempletePattern
+{
+    //Creating Template Method in Abstract Class
+    public abstract class HouseTemplate
+    {
+        public void BuildHouse()
+        {
+            BuildFoundation();
+            BuildPillars();
+            BuildWalls();
+            BuildWindwo();
+        }
+        protected abstract void BuildFoundation();
+        protected abstract void BuildPillars();
+        protected abstract void BuildWalls();
+        protected abstract void BuildWindwo();
+    }
+}
---
+++ TemplatePattern/TempletePattern/TempletePattern/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace TempletePattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Templete Pattern");
+
+            HouseTemplate concreteHouse = new ConcreteHouse();
+            concreteHouse.BuildHouse();
+
+            HouseTemplate woodHouse = new WoodenHouse();
+            woodHouse.BuildHouse();
+        }
+    }
+}
